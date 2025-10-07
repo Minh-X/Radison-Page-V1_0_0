b033ef574f26b8d8d40680691913c303a163913f
mingthedev <tuilajboy@gmail.com>
2025-10-07 12:00:14 +0700

    bento

--- assets/index/index.js
+++ assets/index/index.js
@@ -1,7 +1,12 @@
-const navigationContent = document.querySelector(".navigation__container")
-document.addEventListener("scroll",function(e){
-    navigationResize()
-})
-function navigationResize(){
-    navigationContent.classList.add("__Resize")
+const userName = document.querySelectorAll(".user__name");
+function userNameChangeColor() {
+  for (let i = 0; i < userName.length; i++) {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    userName[i].style.color = `rgb(${r}, ${g}, ${b})`;
+  }
 }
+document.addEventListener("DOMContentLoaded", function (e) {
+  userNameChangeColor();
+});
