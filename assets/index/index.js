c6062c94187f3a97c3796f7cc36bf3782259e489
mingthedev <tuilajboy@gmail.com>
2025-10-17 09:34:03 +0700

    add sth

--- assets/index/index.js
+++ assets/index/index.js
@@ -1,4 +1,8 @@
 const userName = document.querySelectorAll(".user__name");
+const costChange = document.querySelectorAll(".cost");
+const toggleToMonthly = document.querySelector(".monthy__selection")
+const toggleToBack = document.querySelector(".annually__selection")
+const backgroundToggled = document.querySelector(".background__selection")
 function userNameChangeColor() {
   for (let i = 0; i < userName.length; i++) {
     const r = Math.floor(Math.random() * 255);
@@ -10,3 +14,15 @@ function userNameChangeColor() {
 document.addEventListener("DOMContentLoaded", function (e) {
   userNameChangeColor();
 });
+toggleToMonthly.addEventListener("click",function(e){
+  backgroundToggled.style.transform = "translateX(100%)"
+  costChange.forEach(function(e){
+    e.textContent = "$1800"
+  })
+})
+toggleToBack.addEventListener("click",function(e){
+  backgroundToggled.style.transform = null
+  costChange.forEach(function(e){
+    e.textContent = "$480"
+  })
+})
